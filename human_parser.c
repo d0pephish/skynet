16eeb741645c03ea4ed293da74721b6a7f63b05a
ben <jianmin@rainbowsandpwies.com>
2015-09-28 22:23:36 -0400

    passed the ballmer peak. this code is shit.

--- human_parser.c
+++ human_parser.c
@@ -69,7 +69,6 @@ p_nterm * parse_grammer_line( char * , uint32_t , p_nterm *  );
 
 p_term_n * create_p_term_n( char * , uint32_t ); 
 p_gram * build_p_gram( char * , uint32_t ); 
-
 char * clone_str( char * , uint32_t ); 
 
 void free_p_term(p_term * );
@@ -116,29 +115,83 @@ char * clone_str( char * str, uint32_t len )
   memcpy( ret, str, len );
   return ret;
 }
-
+p_nterm * build_p_nterm_single(char * name, uint32_t len)
+{
+  p_nterm * new_p_nterm = (p_nterm *) calloc( 1, sizeof(p_nterm) );
+  new_p_nterm->name = name;
+  new_p_nterm->n_len = len;
+  new_p_nterm->value = NULL;
+  new_p_nterm->len = 0;
+  new_p_nterm->prev = NULL;
+  new_p_nterm->list = create_p_term_n( name , len ); 
+  return new_p_nterm;
+}
 p_gram * build_p_gram( char * sym, uint32_t len) 
 {
   p_gram * new_p_gram = calloc( 1, sizeof(p_gram) );
   new_p_gram->name    = sym;
   new_p_gram->len     = len;
+  p_term_n * create_p_term_n( char * , uint32_t ); 
+  p_gram * build_p_gram( char * , uint32_t ); 
   new_p_gram->nterms  = NULL;
   return new_p_gram;
 }
-
-
+p_term_n * find_nterm_in_list(p_nterm * haystack, char * needle, uint32_t needle_len) {
+  if (haystack == NULL)
+    return NULL;
+  uint32_t len = max(needle_len, haystack->len);
+  int cmp_res = strncmp(needle, haystack->name);
+  
+  if(cmp_res == 0 ) 
+  {
+    return haystack;
+  } else find_nterm_in_list( haystack->prev, needle, needle_len );
+}
+void insert_term_n_into_list( p_term_n * list, p_term_n * term ) 
+{
+  if (list == NULL) 
+    list = term;
+  if (list->t == NULL || term->t = NULL ) 
+  {
+    debug("empty t. this should not happen. check yourself.");
+    return;  
+  }
+  if ( list->t->value == NULL || term->t->value == NULL) 
+  {
+    debug("empty string in t. this shouldn't happen.");
+  }
+  
+  uint32_t max_len = max(term->t->len, list->t->len);
+  int cmp_res = strncmp(term->t->value, term->t->value, max_len);
+  if ( cmp_res == 0 ) 
+  {
+    debug("terminal is already in list. doing nothing.");
+  } else if (cmp_res < 0) 
+  {
+    if ( list->left == NULL ) 
+      list->left == term;
+    else 
+      insert_term_n_into_list( list->left, term ):
+  } else {
+    if ( list->right == NULL )
+      list->right == term;
+    else 
+      insert_term_n_into_list( list->right, term );
+  }
+}
 
 p_nterm * parse_grammer_line( char * line, uint32_t len, p_nterm * p_nterm_list ) 
 {
 
   uint32_t i = 0;
   uint8_t type = TERM;
-
+  p_term_n * cur_p_term_n;
+  p_nterm * cur p_nterm;
   p_nterm * cur = calloc( 1, sizeof( p_nterm ) );  
-
+  
   char word[BUFFSIZE]; 
 
-
+  
   char * line_value = calloc( 1, len );
   memcpy( line_value, line, len );
    
@@ -160,13 +213,20 @@ p_nterm * parse_grammer_line( char * line, uint32_t len, p_nterm * p_nterm_list 
 	      i = 0;
 	      break;
       case ',':
+	      word[i] = '\0';
 	      if( type == TERM ) 
 	      { // build terminal, create pointer to it. 
-          
+          cur_p_term_n = create_p_term_n( (char *) word, i);          
+          insert_term_n_into_list( cur->list, cur_p_term_n );
 	      } else if ( type == NONTERM )
-	      { // find non-terminal in list, create pointer to it
-
+	      { // find non-terminal in list, create pointer to it, or create new
+          cur_p_nterm = find_nterm_in_list(p_nterm_list, word,i); 
+          if( cur_p_nterm == NULL ) {
+            cur_p_term_n = build_p_nterm_single( clone_str ( (char *) word, i ) ;
+          }           
 	      }
+        
+        i = 0; 
         //should set type to TERM after 
         break;
       case '{':
